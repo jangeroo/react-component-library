e1a5556285c2214204c5aee0c9519a339f29b89e
Michael Jang <michaeljang86@gmail.com>
2023-11-24 16:03:38 -0500

    feat: level 1 image slider

--- src/app/image-slider/page.tsx
+++ src/app/image-slider/page.tsx
@@ -1,10 +1,42 @@
 import ImageSlider from '@components/image-slider';
 
+const style = {
+  width: '100%',
+  aspectRatio: '10 / 6'
+}
+
 export default function Page () {
   return (
-    <main>
+    <main style={style}>
       <h1>Image Slider</h1>
-      <ImageSlider />
+      <ImageSlider images={images} />
     </main>
   );
 }
+
+const images = [
+  {
+    src: '/aroids/anthurium-veitchii.jpg',
+    alt: 'anthurium-veitchii.jpg'
+  },
+  {
+    src: '/aroids/epipremnum-cebu-blue.jpg',
+    alt: 'epipremnum-cebu-blue.jpg'
+  },
+  {
+    src: '/aroids/monstera-burle-marx-flame.jpg',
+    alt: 'monstera-burle-marx-flame.jpg'
+  },
+  {
+    src: '/aroids/philodendron-melanochrysum.jpg',
+    alt: 'philodendron-melanochrysum.jpg'
+  },
+  {
+    src: '/aroids/philodendron-spiritus-sancti.jpg',
+    alt: 'philodendron-spiritus-sancti.jpg'
+  },
+  {
+    src: '/aroids/monstera-subpinnata.jpg',
+    alt: 'monstera-subpinnata.jpg'
+  }
+]
--- src/components/image-slider/image-slider.tsx
+++ src/components/image-slider/image-slider.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client'
 
-type Props = {}
+import React, { useState } from 'react'
+import Image from 'next/image'
+import classes from './image-slider.module.css'
 
-export default function ImageSlider ({ }: Props) {
+type Image = {
+  src: string;
+  alt: string;
+}
+
+type Props = {
+  images: Image[]
+}
+
+export default function ImageSlider ({ images }: Props) {
+  const [focusedIndex, setFocusedIndex] = useState(0)
+
+  const showPrevImage = () => {
+    setFocusedIndex(prev => (prev - 1 + images.length) % images.length)
+  }
+
+  const showNextImage = () => {
+    setFocusedIndex(prev => (prev + 1) % images.length)
+  }
+
   return (
-    <div>ImageSlider</div>
+    <div className={classes.wrapper}>
+      <Image
+        className={classes['slider-image']}
+        src={images[focusedIndex].src}
+        alt={images[focusedIndex].alt}
+        width={10000}
+        height={10000}
+      />
+      <button className={`${classes['slider-button']} ${classes.left}`} onClick={showPrevImage} >👈</button>
+      <button className={`${classes['slider-button']} ${classes.right}`} onClick={showNextImage} >👉</button>
+      <div>index: {focusedIndex}</div>
+    </div>
   )
 }

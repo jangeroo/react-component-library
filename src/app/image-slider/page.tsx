843337c8b2c23f3c3235a6a594437e0009cf89e1
Michael Jang <michaeljang86@gmail.com>
2023-11-24 14:18:29 -0500

    chore: component scaffolding

---
+++ src/app/image-slider/page.tsx
@@ -0,0 +1,10 @@
+import ImageSlider from '@components/image-slider';
+
+export default function Page () {
+  return (
+    <main>
+      <h1>Image Slider</h1>
+      <ImageSlider />
+    </main>
+  );
+}
